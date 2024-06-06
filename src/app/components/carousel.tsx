7684b4b3d2629d6ff8c5f0504eaa811ea7896c43
ramlow07 <luamramlow@gmail.com>
2024-06-05 23:04:52 -0300

    adding img carousel autoplay

--- src/app/components/carousel.tsx
+++ src/app/components/carousel.tsx
@@ -11,20 +11,19 @@ export function Carousel() {
     <div className="embla overflow-hidden" ref={emblaRef}>
       <div className="embla__container flex">
         <div className="embla__slide min-w-0 flex-none w-full ">
-          Slide 1
           <img
             src="https://images.unsplash.com/photo-1611365892117-00ac5ef43c90?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Image 1"
           />
         </div>
-        <div className="embla__slide">
+        <div className="embla__slide min-w-0 flex-none w-full ">
           Slide 2
           <img
             src="https://images.unsplash.com/photo-1594818379496-da1e345b0ded?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="slide2"
           />
         </div>
-        <div className="embla__slide">
+        <div className="embla__slide min-w-0 flex-none w-full ">
           Slide 3
           <img
             src="https://plus.unsplash.com/premium_photo-1666345065313-751740249365?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
