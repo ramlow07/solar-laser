67020ed4184b03b19bb19a798210ca9421e9aaf7
ramlow07 <luamramlow@gmail.com>
2024-06-18 22:12:51 -0300

    adding partner comapanies slides

--- src/app/components/carousel.tsx
+++ src/app/components/carousel.tsx
@@ -1,34 +1,36 @@
 "use client";
 import React from "react";
 import useEmblaCarousel from "embla-carousel-react";
-import { useEffect } from "react";
 import Autoplay from "embla-carousel-autoplay";
+import wegLogo from "..public/wegLogo.png";
+import komecoLogo from "..public/komecoLogo.png";
+import intelbrasLogo from "..public/intelbrasLogo.png";
+import { Space_Mono } from "next/font/google";
 
 export function Carousel() {
   const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()]);
 
   return (
-    <div className="embla overflow-hidden" ref={emblaRef}>
-      <div className="embla__container flex">
-        <div className="embla__slide min-w-0 flex-none w-full ">
-          <img
-            src="https://images.unsplash.com/photo-1611365892117-00ac5ef43c90?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Image 1"
-          />
-        </div>
-        <div className="embla__slide min-w-0 flex-none w-full ">
-          Slide 2
-          <img
-            src="https://images.unsplash.com/photo-1594818379496-da1e345b0ded?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="slide2"
-          />
-        </div>
-        <div className="embla__slide min-w-0 flex-none w-full ">
-          Slide 3
-          <img
-            src="https://plus.unsplash.com/premium_photo-1666345065313-751740249365?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="slide3"
-          />
+    // TODO: add style to the h1
+    <div>
+      <h1>
+        Somos credenciados pelas maiores referências em tecnologia solar do
+        Brasil.
+      </h1>
+      <div
+        className="embla mx-auto overflow-hidden max-w-2xl mt-12 mb-12 h-96 border"
+        ref={emblaRef}
+      >
+        <div className="embla__container flex h-full">
+          <div className="embla__slide min-w-0 flex h-full items-center justify-center ">
+            <img src="wegLogo.png" alt="Image 1" />
+          </div>
+          <div className="embla__slide min-w-0 flex w-full items-center justify-center ">
+            <img src="intelbrasLogo.png" alt="slide2" />
+          </div>
+          <div className="embla__slide min-w-0 flex w-full items-center justify-center ">
+            <img src="komecoLogo.png" alt="slide3" />
+          </div>
         </div>
       </div>
     </div>
