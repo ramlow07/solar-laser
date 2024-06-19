16fa3034d777a8621e15d4e5f4e5db8b6965f6cd
ramlow07 <luamramlow@gmail.com>
2024-06-18 22:38:18 -0300

    adding title to the brands carousel

--- src/app/components/carousel.tsx
+++ src/app/components/carousel.tsx
@@ -13,10 +13,20 @@ export function Carousel() {
   return (
     // TODO: add style to the h1
     <div>
-      <h1>
-        Somos credenciados pelas maiores referências em tecnologia solar do
-        Brasil.
+      <h1 class="font-inter flex items-center justify-center mb-4 text-xl font-semibold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-3xl dark:text-white">
+        Trabalhamos com as{" "}
+        <span class="text-orange-600 dark:text-black-500 mx-2">
+          maiores referências
+        </span>{" "}
+        de produtos em energia solar do Brasil.
       </h1>
+      <p className=" mt-3 mx-5 font-inter flex items-center justify-center tracking-tight">
+        Seu negócio, nosso compromisso. Para podermos melhor atender às
+        necessidades de sua propriedade ou empresa, a qualidade precisa ser
+        garantida desde os primeiros passos. E por isso, trabalhamos com as
+        melhores marcas do Brasil, das quais somos também credenciados.
+      </p>
+
       <div
         className="embla mx-auto overflow-hidden max-w-2xl mt-12 mb-12 h-96 border"
         ref={emblaRef}
