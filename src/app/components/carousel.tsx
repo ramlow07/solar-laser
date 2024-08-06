51f73b7494df0004b740fbd67b8127fe2e87f556
ramlow07 <luamramlow@gmail.com>
2024-08-05 21:02:26 -0300

    updating carousel and starting feedbackSection

--- src/app/components/carousel.tsx
+++ src/app/components/carousel.tsx
@@ -2,11 +2,6 @@
 import React from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
-import wegLogo from "..public/wegLogo.png";
-import komecoLogo from "..public/komecoLogo.png";
-import intelbrasLogo from "..public/intelbrasLogo.png";
-import { Space_Mono } from "next/font/google";
-import { Inter } from "next/font/google";
 
 export function Carousel() {
   const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()]);
@@ -14,7 +9,7 @@ export function Carousel() {
   return (
     // TODO: add style to the h1
     <div className="">
-      <h1 className="font-sans flex items-center justify-center mb-4 text-xl font-semibold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-3xl dark:text-white">
+      <h1 className="font-sans flex items-center justify-center mb-4 text-xl font-semibold leading-none tracking-tight text-gray-900 md:text-2xl sm:text-xl lg:text-3xl dark:text-white">
         Trabalhamos com as maiores referências de produtos em energia solar do
         Brasil.
       </h1>
@@ -26,15 +21,15 @@ export function Carousel() {
       </p>
 
       <div
-        className="embla mx-auto overflow-hidden max-w-2xl mt-12 mb-12 h-96 border"
+        className="embla mx-auto overflow-hidden max-w-10xl mt-12 mb-12 h-40 border"
         ref={emblaRef}
       >
         <div className="embla__container flex h-full">
-          <div className="embla__slide min-w-0 flex h-full items-center justify-center ">
-            <img src="wegLogo.png" alt="Image 1" />
-          </div>
           <div className="embla__slide min-w-0 flex w-full items-center justify-center ">
-            <img src="intelbrasLogo.png" alt="slide2" />
+            <img src="wegLogo1.png" alt="Image 1" />
+          </div>
+          <div className="embla__slide min-w-0 flex h-full items-center justify-center ">
+            <img src="intelbrasLogo1.png" alt="slide2" />
           </div>
           <div className="embla__slide min-w-0 flex w-full items-center justify-center ">
             <img src="komecoLogo.png" alt="slide3" />
--- src/app/components/feedbackCarousel.tsx
+++ src/app/components/feedbackCarousel.tsx
@@ -2,17 +2,11 @@
 import React from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
-import wegLogo from "..public/wegLogo.png";
-import komecoLogo from "..public/komecoLogo.png";
-import intelbrasLogo from "..public/intelbrasLogo.png";
-import { Space_Mono } from "next/font/google";
-import { Inter } from "next/font/google";
 
 export function FeedbackCarousel() {
   const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()]);
 
   return (
-    // TODO: add style to the h1
     // TODO: add bv logo inside the slider
     <div className="">
       <h1 className="font-sans flex items-center justify-center mb-4 text-xl font-normal bg-white leading-none tracking-tight text-gray-900 md:text-3xl lg:text-3xl dark:text-white">
@@ -20,7 +14,7 @@ export function FeedbackCarousel() {
       </h1>
 
       <div
-        className="embla mx-auto overflow-hidden max-w-2xl mt-12 mb-12 h-48 border"
+        className="embla mx-auto overflow-hidden max-w-10xl mt-12 mb-12 h-40 border"
         ref={emblaRef}
       >
         <div className="embla__container flex h-full">
